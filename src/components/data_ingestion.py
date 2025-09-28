632a295b935e3542ba439c9b3e73ac6b76c51ff0
Dipesh Ghimire <dipeshghimire.dg@gmail.com>
2025-09-28 14:06:24 +0545

    save pickle file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
@@ -28,7 +30,11 @@ class DataIngestion:
         try:
             df = pd.read_csv("notebook/data/retail_sales_data.csv")
             #Data can be read from any sources like csv,database(mongodb,mysql)
-            
+            # Handling Some Features
+            df = df.drop(columns=['Item_Identifier'])
+            df['Item_Fat_Content'] = df['Item_Fat_Content'].replace({
+                'LF': 'Low Fat', 'low fat': 'Low Fat', 'reg': 'Regular'
+            })
             logging.info("Successfully read dataset as df")
             
             #create artifact directory
@@ -39,8 +45,8 @@ class DataIngestion:
             logging.info("Train Test Split Initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             
-            train_set.to_csv(self.ingestion_config.train_data_path)
-            test_set.to_csv(self.ingestion_config.test_data_path)
+            train_set.to_csv(self.ingestion_config.train_data_path ,index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
             logging.info("Data Ingestion Completed")
 
             return (self.ingestion_config.train_data_path,self.ingestion_config.test_data_path)
@@ -51,4 +57,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
