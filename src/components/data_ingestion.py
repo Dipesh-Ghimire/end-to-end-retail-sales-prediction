830d79632bf250ded760b3c4da6ebb696655d056
Dipesh Ghimire <dipeshghimire.dg@gmail.com>
2025-09-28 17:32:34 +0545

    raise some custom exception

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -52,7 +52,7 @@ class DataIngestion:
             return (self.ingestion_config.train_data_path,self.ingestion_config.test_data_path)
 
         except Exception as e:
-            CustomException(e,sys)
+            raise CustomException(e,sys)
 
 
 if __name__=="__main__":
